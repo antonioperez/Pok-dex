d1cbdecf56242c9fbd4d56eb8487d89d0c663c9e
Antonio Perez <aperez2541@gmail.com>
2023-11-29 23:26:24 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { HotToaster } from '../components/hot-toaster'
 
 export const metadata = {
   metadataBase: new URL('https://pokedex-seven-sigma.vercel.app'),
-  title: 'Pokedex AI-powered Semantic Search Demo',
+  title: 'Pokedex Semantic Search Demo',
   description:
     'A Next.js app that uses Vercel Postgres with pgvector, Prisma, and OpenAI to power a semantic search.',
 }
